1cd361d6eae44a664147d1e01d5364a4147fb428
vmartinir <vmartinir@aliyun.com>
2022-07-30 11:43:02 +0800

    Update datasource module

--- spring-distributed-datasource/src/main/java/org/example/Application.java
+++ spring-distributed-datasource/src/main/java/org/example/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("org.example.example.repository")
+@MapperScan("org.example.repository")
 public class Application {
 
     public static void main(String[] args) {

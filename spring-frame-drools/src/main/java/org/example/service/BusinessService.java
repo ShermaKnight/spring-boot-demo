9f60fb5af0ddc8040082ebee2532553a796a9701
vmartinir <vmartinir@aliyun.com>
2023-01-28 15:36:04 +0800

    Update drools module

---
+++ spring-frame-drools/src/main/java/org/example/service/BusinessService.java
@@ -0,0 +1,10 @@
+package org.example.service;
+
+import org.example.domain.vo.BusinessResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface BusinessService {
+
+    BusinessResponse getInformation(HttpServletRequest servletRequest);
+}
